4b946b09f756add5257e353d88e537f4495508ce
OoI3enZaoO <Teerapath-Ben@hotmail.com>
2018-11-23 17:42:53 +0700

    update: add dockerFile & add compression module

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const restify = require('restify');
 const server = restify.createServer();
 const PORT = 5000 || process.env.PORT
 const morgan = require('morgan')
+const compression = require('compression')
 
 server.use(restify.plugins.bodyParser());
 server.use(morgan('dev'))
+server.use(compression())
 
 require('./routes/members.js')(server)
 require('./routes/auth.js')(server)
